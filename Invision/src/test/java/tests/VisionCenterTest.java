22fc253cad93f4fd7d586d286902a53f7d61a89a
rohit kumar <138009969+Rohitkr41@users.noreply.github.com>
2026-03-06 14:15:43 +0530

    Update VisionCenterTest.java
    updated code

--- Invision/src/test/java/tests/VisionCenterTest.java
+++ Invision/src/test/java/tests/VisionCenterTest.java
@@ -1,19 +1,15 @@
-
-
 package tests;
 
 import base.BaseTest;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.LoginPage;
 import pages.SidebarPage;
-import pages.RegistrationPage;
 import utils.ConfigReader;
 
 public class VisionCenterTest extends BaseTest {
 
     @Test
-    public void openRegistrationFromVisionCenter() {
+    public void openVisionCenter() {
 
         // Login
         LoginPage login = new LoginPage(driver);
@@ -25,15 +21,5 @@ public class VisionCenterTest extends BaseTest {
         // Navigation
         SidebarPage sidebar = new SidebarPage(driver);
         sidebar.clickVisionCenter();
-        sidebar.clickRegistration();
-
-        // Validation
-        RegistrationPage registrationPage =
-                new RegistrationPage(driver);
-
-        Assert.assertTrue(
-                registrationPage.isRegistrationPageDisplayed(),
-                "Registration page not opened"
-        );
     }
 }
